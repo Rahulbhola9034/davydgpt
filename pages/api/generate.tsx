dfaacf11b6cb34a7634183c13ac948d197e93b9f
Rahulbhola9034 <Rahulbhola9034@gmail.com>
2022-12-14 16:30:04 +0530

    first commit

---
+++ pages/api/generate.tsx
@@ -0,0 +1,26 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function (req, res) {
+  const { search } = req.body;
+  console.log(search);
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: generatePrompt(search),
+    temperature: 0.6,
+    max_tokens: 2048,
+  });
+  console.log(completion.data.choices[0].text);
+  res.status(200).json({ result: completion.data.choices[0].text });
+}
+
+function generatePrompt(search) {
+  const capitalizedAnimal =
+    search[0].toUpperCase() + search.slice(1).toLowerCase();
+  console.log(search);
+  return search;
+}
